4a74d6acdf23e50f2b910e9a526133ece2545f8d
ArundhatiKundap <aruvibh1887@gmail.com>
2025-06-09 08:20:22 -0500

    write appropriate methods in calculator and StringUtilities

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -8,4 +8,19 @@ public class Calculator {
     // Add a static function called multiply that takes two int parameters and returns their product (as an int)
 
     // Add a static function called divide that takes two int parameters and returns their quotient (as an int)
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
 }
--- src/main/java/org/example/StringUtilities.java
+++ src/main/java/org/example/StringUtilities.java
@@ -13,4 +13,20 @@ public class StringUtilities {
 
     // Add a static function called bigger that takes two String parameters and returns the longer of the two strings. If the
     // strings are the same length, return the first string.
+    public static boolean shortString(String givenString){
+        return givenString.length() < 5;
+    }
+    public static char firstLetter(String givenString){
+        return givenString.charAt(0);
+    }
+    public static String censorAsparagus(String givenString){
+        return givenString.replace("asparagus", "****");
+    }
+    public static String bigger(String firstSting,String secondString){
+        if(firstSting.length()>secondString.length() || firstSting.length()==secondString.length()){
+            return firstSting;
+        }else{
+            return secondString;
+        }
+    }
 }
